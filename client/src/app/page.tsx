50c97889a863e284336e6817c94eb2f64eae64d0
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-01-06 21:58:18 +0530

    feat(client): add dummy functionality

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,38 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Zap } from "lucide-react";
+import { Loader2, Zap } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+	const [videoLink, setVideoLink] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleGenerateBlog = async () => {
+		// Call the API to generate blog
+		// const response = await fetch("/api/generate-blog", {
+		// 	method: "POST",
+		// 	body: JSON.stringify({ videoLink }),
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// });
+
+		// if (response.ok) {
+		// 	const { blog } = await response.json();
+		// 	console.log(blog);
+		// } else {
+		// 	console.error("Failed to generate blog");
+		// }
+		setIsLoading(true);
+		setTimeout(() => {
+			console.log("Blog generated successfully!");
+			setIsLoading(false);
+		}, 2000);
+		console.log("Generating blog from video link:", videoLink);
+	};
+
 	return (
 		<main className="max-w-[1100px] mx-auto px-4 my-14">
 			<h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-center">
@@ -13,9 +43,25 @@ export default function Home() {
 				blog created from it!
 			</p>
 			<div className="max-w-[600px] mx-auto mt-8 space-y-6">
-				<Input placeholder="YouTube video link" />
-				<Button className="w-full">
-					Generate blog <Zap fill="#fff" strokeWidth={0} />
+				<Input
+					placeholder="YouTube video link"
+					value={videoLink}
+					onChange={e => setVideoLink(e.target.value)}
+				/>
+				<Button
+					className="w-full"
+					onClick={handleGenerateBlog}
+					disabled={isLoading}
+				>
+					{isLoading ? (
+						<>
+							Generating <Loader2 className="animate-spin" />
+						</>
+					) : (
+						<>
+							Generate blog <Zap fill="#fff" strokeWidth={0} />
+						</>
+					)}
 				</Button>
 			</div>
 		</main>
