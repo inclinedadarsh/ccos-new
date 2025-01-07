a409aca1429a6ab607cc3ac157120845f3df72aa
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-01-07 17:51:36 +0530

    feat(client): enhance ui of blog preview

--- client/src/components/Blog.tsx
+++ client/src/components/Blog.tsx
@@ -1,3 +1,4 @@
+import { BookMarked } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
@@ -7,20 +8,28 @@ const Blog = ({
 	setBlog,
 }: { blog: string; setBlog: React.Dispatch<React.SetStateAction<string>> }) => {
 	return (
-		<div className="mt-10 p-4 border rounded markdown">
+		<div className="mt-10 rounded-lg shadow-lg p-6 border border-border ">
+			<div className="flex items-center gap-2 mb-4">
+				<BookMarked size={20} className="" />
+				<h2 className="text-xl font-medium">Generated Blog Post</h2>
+			</div>
 			<Tabs defaultValue="preview">
 				<TabsList className="grid w-full grid-cols-2">
 					<TabsTrigger value="preview">Preview</TabsTrigger>
 					<TabsTrigger value="edit">Edit</TabsTrigger>
 				</TabsList>
 				<TabsContent value="preview">
-					<ReactMarkdown remarkPlugins={[remarkGfm]}>
-						{blog}
-					</ReactMarkdown>
+					<div className="bg-gray-50 p-4 border-border border-2 border-dashed rounded-md max-h-[500px] min-h-[300px] overflow-auto">
+						<div className="markdown">
+							<ReactMarkdown remarkPlugins={[remarkGfm]}>
+								{blog}
+							</ReactMarkdown>
+						</div>
+					</div>
 				</TabsContent>
 				<TabsContent value="edit">
 					<textarea
-						className="w-full h-96 p-4 text-black"
+						className="bg-gray-50 p-4 border-border border-2 border-dashed rounded-md w-full max-h-[500px] min-h-[300px] resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 overflow-auto font-mono"
 						value={blog}
 						onChange={e => {
 							setBlog(e.target.value);
