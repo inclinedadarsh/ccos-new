55ecd2bafb917042d2c18ffda6f831bd86afb43e
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-01-11 01:04:58 +0530

    fix(server): add any origin to allow server access

--- server/src/main.py
+++ server/src/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://ccos-ia.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
